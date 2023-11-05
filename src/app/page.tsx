be689e558df893a17ae724fcde33b123abf9ddd8
Rajat Bhatt <rajatbhatt999@gmail.com>
2023-11-05 14:10:18 -0500

    Add react eslint plugins

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,11 @@
 import Image from "next/image";
 
-export default function Home() {
+export default async function Home() {
+  const dict = {
+    abc: 1,
+    hello: "world",
+  };
+  console.log(dict);
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
